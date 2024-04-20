6c6f7300e26b3b4226d9ef2a649ff91da1b9d562
Zhen <61930828+master-z33@users.noreply.github.com>
2024-04-20 20:46:16 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+function alert() {
+        Swal.fire({
+  title: "Sweet!",
+  text: "Modal with a custom image.",
+  imageUrl: "https://unsplash.it/400/200",
+  imageWidth: 400,
+  imageHeight: 200,
+  confirmButtonText: "false",             
+  imageAlt: "Custom image"
+});
